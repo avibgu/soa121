94dcb1fc237d5fc9b3569579c1eec73466ab21d3
amarnirm@gmail.com <amarnirm@gmail.com@f417045c-efef-7132-a31b-4cff1c5038f5>
2012-02-26 06:06:40 +0000

    canceled the '+' button for adding new Tags. Tag will  be added by separated comma list. (Tag1, Tag2, Tag3 ..)

--- trunk/SOA-Project/html/scripts.js
+++ trunk/SOA-Project/html/scripts.js
@@ -1,31 +1,6 @@
 MAX_TAGS = 5;
 
 // add another field tag to add Posts
-function addTagFieldNewPost( whereToAddId )
-{	
-	if ( typeof tagFieldNum == 'undefined' ) {
-		tagFieldNum = 1;
-    }
-		
-	if(tagFieldNum > MAX_TAGS - 1)
-		return;
-
-	tagFieldNum++;
-			
-	finalString = "";
-	for(i =1; i<= tagFieldNum; i++){
-		finalString = finalString + '<input type="search" name="autor" id="authorInputTagList" value="" placeholder="Tag"/>';
-	}
-
-	$(whereToAddId).html(finalString);
-		
-	if(tagFieldNum > MAX_TAGS - 1){
-		jQuery("#addTagBtnForNew").attr('disabled', 'disabled');
-	}
-}
-
-
-// add another field tag to add Posts
 function addTagFieldReadPosts( whereToAddId )
 {	
 	if ( typeof tagFieldNum == 'undefined' ) {
